94b613a716b9fd692c7b8983bf38a1857c3b0ce0
Ahmed Sarhan <ahmed.sarhan@swvl.com>
2022-05-09 17:48:04 +0200

    adding the api context

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { useApi } from './api';
+import { useQuery } from 'react-query';
+
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// calling the useApi hook created from the api context;
+	const api = useApi();
+
+	const [betIdState, updateBetIdState] = useState<number>(1);
+
+	// the fetch function using the generated types and schema from api context
+	const fetchBetById = async (betId: number) => {
+		return await api.getPetById(betId);
+	};
+
+	// passing the fetch function to useQuery hook to fetch through react query
+	// this will always give 404 due to the fact this example doesn't have a BE url
+	const { data, isFetching, isError } = useQuery(
+		['fetchSingleBet', betIdState],
+		() => fetchBetById(betIdState)
+	);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApiProvider } from './api';
+import { QueryClient, QueryClientProvider } from 'react-query';
+const REACT_APP_API_URL = 'api url here';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+const client = new QueryClient();
+ReactDOM.render(
+	<React.StrictMode>
+		<QueryClientProvider client={client}>
+			<ApiProvider url={process.env.REACT_APP_API_URL! || REACT_APP_API_URL}>
+				<App />
+			</ApiProvider>
+		</QueryClientProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
